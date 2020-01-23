b57cd6c89ded2b73eae66d7fe27ae087aad0b37b
g.lagier <gaetan.lagier@hotmail.fr>
2020-01-23 21:25:48 +0100

    use simple lighten color method

--- src/components/SvgVoxelEngine.vue
+++ src/components/SvgVoxelEngine.vue
@@ -142,127 +142,34 @@ export default {
       const [p1, , p3, p4, p5, p6, p7, p8] = this.getVoxelCoordinates(position);
       return `
       <g>
-        ${this.makeFace([p5, p6, p7, p8], this.lightenColor(color, 0.1))}
+        ${this.makeFace([p5, p6, p7, p8], this.lightenColor(color, 10))}
         ${this.makeFace([p1, p5, p8, p4], color)}
-        ${this.makeFace([p8, p7, p3, p4], this.darkenColor(color, 0.4))}
+        ${this.makeFace([p8, p7, p3, p4], this.darkenColor(color, 40))}
       </g>`;
     },
     darkenColor(color, amount) {
-      return this.pSBC(-amount, color);
-    },
-    lightenColor(color, amount) {
-      return this.pSBC(amount, color);
+      return this.lightenColor(color, -amount);
     },
     /**
-     * From https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+     * From https://gist.github.com/renancouto/4675192
      */
-    pSBC(p, c0, c1, l) {
-      let r,
-        g,
-        b,
-        P,
-        f,
-        t,
-        h,
-        i = parseInt,
-        m = Math.round,
-        a = typeof c1 == "string";
-      if (
-        typeof p != "number" ||
-        p < -1 ||
-        p > 1 ||
-        typeof c0 != "string" ||
-        (c0[0] != "r" && c0[0] != "#") ||
-        (c1 && !a)
-      )
-        return null;
-      if (!this.pSBCr)
-        this.pSBCr = d => {
-          let n = d.length,
-            x = {};
-          if (n > 9) {
-            ([r, g, b, a] = d = d.split(",")), (n = d.length);
-            if (n < 3 || n > 4) return null;
-            (x.r = i(r[3] == "a" ? r.slice(5) : r.slice(4))),
-              (x.g = i(g)),
-              (x.b = i(b)),
-              (x.a = a ? parseFloat(a) : -1);
-          } else {
-            if (n == 8 || n == 6 || n < 4) return null;
-            if (n < 6)
-              d =
-                "#" +
-                d[1] +
-                d[1] +
-                d[2] +
-                d[2] +
-                d[3] +
-                d[3] +
-                (n > 4 ? d[4] + d[4] : "");
-            d = i(d.slice(1), 16);
-            if (n == 9 || n == 5)
-              (x.r = (d >> 24) & 255),
-                (x.g = (d >> 16) & 255),
-                (x.b = (d >> 8) & 255),
-                (x.a = m((d & 255) / 0.255) / 1000);
-            else
-              (x.r = d >> 16),
-                (x.g = (d >> 8) & 255),
-                (x.b = d & 255),
-                (x.a = -1);
-          }
-          return x;
-        };
-      (h = c0.length > 9),
-        (h = a ? (c1.length > 9 ? true : c1 == "c" ? !h : false) : h),
-        (f = this.pSBCr(c0)),
-        (P = p < 0),
-        (t =
-          c1 && c1 != "c"
-            ? this.pSBCr(c1)
-            : P
-            ? { r: 0, g: 0, b: 0, a: -1 }
-            : { r: 255, g: 255, b: 255, a: -1 }),
-        (p = P ? p * -1 : p),
-        (P = 1 - p);
-      if (!f || !t) return null;
-      if (l)
-        (r = m(P * f.r + p * t.r)),
-          (g = m(P * f.g + p * t.g)),
-          (b = m(P * f.b + p * t.b));
-      else
-        (r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5)),
-          (g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5)),
-          (b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5));
-      (a = f.a),
-        (t = t.a),
-        (f = a >= 0 || t >= 0),
-        (a = f ? (a < 0 ? t : t < 0 ? a : a * P + t * p) : 0);
-      if (h)
-        return (
-          "rgb" +
-          (f ? "a(" : "(") +
-          r +
-          "," +
-          g +
-          "," +
-          b +
-          (f ? "," + m(a * 1000) / 1000 : "") +
-          ")"
-        );
-      else
-        return (
-          "#" +
-          (
-            4294967296 +
-            r * 16777216 +
-            g * 65536 +
-            b * 256 +
-            (f ? m(a * 255) : 0)
-          )
-            .toString(16)
-            .slice(1, f ? undefined : -2)
-        );
+    lightenColor(color, percent) {
+      const num = parseInt(color.replace("#", ""), 16);
+      const amt = Math.round(2.55 * percent);
+      const R = (num >> 16) + amt;
+      const B = ((num >> 8) & 0x00ff) + amt;
+      const G = (num & 0x0000ff) + amt;
+      return (
+        "#" +
+        (
+          0x1000000 +
+          (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+          (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 +
+          (G < 255 ? (G < 1 ? 0 : G) : 255)
+        )
+          .toString(16)
+          .slice(1)
+      );
     },
     makeFace(points, color) {
       const [p1, p2, p3, p4] = points;
