b4f1975b4e04f91ab4344a11e2aabec29c903c4e
g.lagier <gaetan.lagier@hotmail.fr>
2020-01-23 17:05:14 +0100

    cleanup vue-cli generated code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SvgVoxelEngine />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SvgVoxelEngine from "./components/SvgVoxelEngine.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    SvgVoxelEngine
   }
 };
 </script>
---
+++ src/components/SvgVoxelEngine.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="svgVoxelEngine"></div>
+</template>
+
+<script>
+export default {
+  name: "SvgVoxelEngine"
+};
+</script>
+
+<style scoped lang="scss"></style>
